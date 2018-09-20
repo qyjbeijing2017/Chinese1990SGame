e090bffbb334171b968f74d0b976c5dabbc116b4
qyjbeijing2017 <qyjbeijing@163.com>
2018-09-20 13:41:36 +0800

    behaviac 重新导入

---
+++ PaperWife/Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    [SerializeField] private int a;
+    [Space(10)] public int b;
+    [Header("这个是个测试")] public int c;
+    [Header("1")]
+    [SerializeField, Range(0, 10),Tooltip("d is a test")] private float d;
+
+    public int A
+    {
+        get
+        {
+            return a;
+        }
+        set
+        {
+            Update();
+            a = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
