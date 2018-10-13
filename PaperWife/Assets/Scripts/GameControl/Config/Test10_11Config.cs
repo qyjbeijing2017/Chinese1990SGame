45006d2d80b2f827a3dfc79c928f9223fcb5abd4
Gryonder <42128987+Gryonder@users.noreply.github.com>
2018-10-13 17:54:08 +0800

    表导入测试

---
+++ PaperWife/Assets/Scripts/GameControl/Config/Test10_11Config.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DaemonTools;
+using UnityEngine;
+public class Test10_11Config : BaseConfig {
+	public int ID;
+	public string Fortest1;
+
+	public float fortest2;
+	public override void InitConfig (List<string> m_data) {
+		int.TryParse(m_data[0],out ID);
+		Fortest1 = m_data[1];
+		float.TryParse(m_data[2], out fortest2);
+	}
+
+}
--- PaperWife/Assets/Scripts/GameControl/LogicTool/ConfigManager.cs
+++ PaperWife/Assets/Scripts/GameControl/LogicTool/ConfigManager.cs
@@ -8,14 +8,17 @@ namespace DaemonTools
     {
         //声明配置表名
         const string TestConfigName = "test";
+        const string Test10_11ConfigName = "test10_11";
         
         //声明配置表存储
         public Dictionary<int, TestConfig> TestConfigData;
+        public Dictionary<int, Test10_11Config> Test10_11ConfigData;
 
         //实例化配置表
         public void InitConfigManager()
         {
             TestConfigData = ConfigFactory<TestConfig>.InitConfigs(TestConfigName);
+            Test10_11ConfigData = ConfigFactory<Test10_11Config>.InitConfigs(Test10_11ConfigName);
         }
 
     }
