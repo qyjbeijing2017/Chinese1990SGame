519860901a752ad10e5f72911dff6c3eb9bfa579
Gryonder <42128987+Gryonder@users.noreply.github.com>
2018-12-18 18:52:37 +0800

    按键绑定，物体吸引

--- PaperWife/Assets/Scripts/Player/MagnetPlayerController.cs
+++ PaperWife/Assets/Scripts/Player/MagnetPlayerController.cs
@@ -14,10 +14,60 @@ public class MagnetPlayerController : MonoBehaviour {
 	[SerializeField]private BoxCollider2D m_lowerRight;
 	
 	private Rigidbody2D m_rigidbody;
-	[SerializeField]private int m_force = 3; 
-	[SerializeField]private List <bool> m_boxCollider2D = new List<bool>();
+	[SerializeField]private int m_playerID = 1;
+	[SerializeField]private int m_playerForce = 10; 
+	[SerializeField]private int m_magnetForce = 30; 
+	[SerializeField]private bool isPositive = true;
+	 
+	 private float m_joystickV;
+	 private float m_joystickH;
+	 private float m_joystickRT;
+
+	/* [SerializeField]private List <Collider2D> m_boxCollider2D = new List<Collider2D>();
+
+	enum MyCollider
+	{
+		none = 0,
+		up = 1,
+		down = 2,
+		left = 3,
+		right = 4,
+		upperleft = 5,
+		upperright = 6,
+		lowerleft = 7,
+		lowerright = 8,
+	}
+	 Dictionary<MyCollider, Collider2D> myCollider = new Dictionary<MyCollider, Collider2D>();*/
 	void Start () {
 		m_rigidbody = this.GetComponent<Rigidbody2D>();
+		if (m_playerID == 1)
+		{
+			m_joystickH = Input.GetAxis("Joystick1H");
+			m_joystickV = Input.GetAxis("Joystick1V");
+			m_joystickRT = Input.GetAxis("Joystick1RT");
+			return;
+		}
+		if (m_playerID == 2)
+		{
+			m_joystickH = Input.GetAxis("Joystick2H");
+			m_joystickV = Input.GetAxis("Joystick2V");
+			m_joystickRT = Input.GetAxis("Joystick2RT");
+			return;
+		}
+		if (m_playerID == 3)
+		{
+			m_joystickH = Input.GetAxis("Joystick3H");
+			m_joystickV = Input.GetAxis("Joystick3V");
+			m_joystickRT = Input.GetAxis("Joystick3RT");
+			return;
+		}
+		if (m_playerID == 4)
+		{
+			m_joystickH = Input.GetAxis("Joystick4H");
+			m_joystickV = Input.GetAxis("Joystick4V");
+			m_joystickRT = Input.GetAxis("Joystick4RT");
+			return;
+		}
 	}
 	
 	// Update is called once per frame
@@ -25,51 +75,62 @@ public class MagnetPlayerController : MonoBehaviour {
 		move();
 		ColliderOn();
 	}
+
 	void Update() {
 		ColliderOff();
 	}
+
+
 	void move(){
 		float h = Input.GetAxis("Horizontal");
-		m_rigidbody.AddForce(new Vector2(h,0) * m_force);
+		m_rigidbody.AddForce(new Vector2(h,0) * m_playerForce);
+		
 	}
 	//移动脚本
+
 	void ColliderOn(){
-		if(Input.GetKeyDown(KeyCode.I)){
-			if(Input.GetKeyDown(KeyCode.J)){
+		
+		if(Input.GetKeyDown(KeyCode.I) || m_joystickV > 0){
+			if(Input.GetKeyDown(KeyCode.J) || m_joystickH < 0){
 				m_upperLeft.enabled = true;
 				return;
 			}
-			if (Input.GetKeyDown(KeyCode.L)){
+			if (Input.GetKeyDown(KeyCode.L) || m_joystickH > 0){
 				m_upperRight.enabled = true;
 				return;
 			}
 			m_up.enabled = true;
 			return;
 		}	
-		if (Input.GetKeyDown(KeyCode.K)){
-			if(Input.GetKeyDown(KeyCode.J)){
+		if (Input.GetKeyDown(KeyCode.K) || m_joystickV < 0){
+			if(Input.GetKeyDown(KeyCode.J) || m_joystickH < 0){
 				m_lowerLeft.enabled = true;
 				return;
 			}
-			if(Input.GetKeyDown(KeyCode.L)){
+			if(Input.GetKeyDown(KeyCode.L) || m_joystickH > 0){
 				m_lowerRight.enabled = true;
 				return;
 			}
 			m_down.enabled = true;
 			return;
 		}
-		if(Input.GetKeyDown(KeyCode.J)){
+		if(Input.GetKeyDown(KeyCode.J) || m_joystickH < 0){
 			m_left.enabled = true;
 			return;
 		}
-		if(Input.GetKeyDown(KeyCode.L)){
+		if(Input.GetKeyDown(KeyCode.L) || m_joystickH > 0){
 			m_right.enabled = true;
 			return;
 		}
 							
 	}
+	private void OnTriggerEnter2D(Collider2D Other) {
+	
+			Other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(this.gameObject.transform.position.x,this.gameObject.transform.position.y) * m_magnetForce);
+	
+	}
 	void ColliderOff(){
-		if (m_up.enabled == true )
+	if (m_up.enabled == true )
 		{
 			m_up.enabled = false;
 		}
@@ -102,6 +163,44 @@ public class MagnetPlayerController : MonoBehaviour {
 			m_lowerRight.enabled = false;
 		}
 		//碰撞盒关闭
+		/* var dicenumator = myCollider.GetEnumerator();
+		while(dicenumator.MoveNext()){
+			if (dicenumator.Current.Key == MyCollider.up){
+				dicenumator.Current.Value.enabled = false;
+			}
+			if (dicenumator.Current.Key == MyCollider.down)
+			{
+				dicenumator.Current.Value.enabled = false;
+			}
+			if (dicenumator.Current.Key == MyCollider.down)
+			{
+				dicenumator.Current.Value.enabled = false;
+			}
+			if (dicenumator.Current.Key == MyCollider.left)
+			{
+				dicenumator.Current.Value.enabled = false;
+			}	
+			if (dicenumator.Current.Key == MyCollider.right)
+			{
+				dicenumator.Current.Value.enabled = false;
+			}
+			if (dicenumator.Current.Key == MyCollider.upperleft)
+			{
+				dicenumator.Current.Value.enabled = false;
+			}
+			if (dicenumator.Current.Key == MyCollider.upperright)
+			{
+				dicenumator.Current.Value.enabled = false;
+			}
+			if (dicenumator.Current.Key == MyCollider.lowerleft)
+			{
+				dicenumator.Current.Value.enabled = false;
+			}
+			if (dicenumator.Current.Key == MyCollider.lowerright)
+			{
+				dicenumator.Current.Value.enabled = false;
+			}
+		}*/
 	
 	}
 
