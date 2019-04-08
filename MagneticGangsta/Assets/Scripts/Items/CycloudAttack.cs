b80994cd04ae799330272bccff58e162eee2d2ec
qyj <qyjbeijing@163.com>
2019-04-08 18:32:51 +0800

    设置初始值

--- MagneticGangsta/Assets/Scripts/Items/CycloudAttack.cs
+++ MagneticGangsta/Assets/Scripts/Items/CycloudAttack.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class CycloudAttack : AttackBase
 {
+    public override void PlayerInit()
+    {
+        if (!collider)
+            collider = GetComponent<CircleCollider2D>() as Collider2D;
 
+
+        AttackDamage.Attacker = this;
+    }
 
     public override void PlayerLoop()
     {
