b6dfb46e75f459206b435842a0e41689c62bb815
qyj <qyjbeijing@163.com>
2019-04-10 13:05:27 +0800

    爆炸部分效果

---
+++ MagneticGangsta/Assets/Scripts/Items/ItemBoom.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ItemBoom : PlayerFunctionBase
+{
+
+    public event UnityAction OnCreatItem;
+
+    [SerializeField] private Animator animator;
+    
+    // Start is called before the first frame update
+    public override void PlayerInit()
+    {
+        if (animator == null)
+        {
+            animator.GetComponent<Animation>();
+        }
+    }
+
+    public void BoomStart()
+    {
+
+    }
+
+    public void CreatItem()
+    {
+        OnCreatItem?.Invoke();
+    }
+
+    public void BoomEnd()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ MagneticGangsta/Assets/Scripts/Items/MeteoriteFall.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoriteFall : PlayerFunctionBase
+{
+    public Vector2 TargetPoint;
+
+    public float Speed = 1f;
+
+    public float SpeedAdd = 3f;
+
+    private Vector2 Pos2Target
+    {
+        get
+        {
+            return TargetPoint - (Vector2)transform.position;
+        }
+    }
+
+    [SerializeField]private Collider2D collider;
+
+    public override void PlayerInit()
+    {
+        if (collider == null)
+        {
+            collider = GetComponent<Collider2D>();
+        }
+        transform.forward = Pos2Target;
+        collider.isTrigger = true;
+    }
+
+    public override void PlayerLoop()
+    {
+        Speed += SpeedAdd * Time.deltaTime;
+        transform.position += (Vector3)Pos2Target.normalized * Speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
