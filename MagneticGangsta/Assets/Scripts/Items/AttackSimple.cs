85ab4a14531af2647ab826d5e8ce6da25ed567d3
qyj <qyjbeijing@163.com>
2019-04-10 15:27:17 +0800

    修复手柄bug

---
+++ MagneticGangsta/Assets/Scripts/Items/AttackSimple.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSimple : PlayerFunctionBase
+{
+    public DamageBase AttackDamage = new DamageBase();
+
+    public bool IsAttack;
+
+    public Collider2D collider;
+
+
+
+    public override void PlayerInit()
+    {
+        if (collider != null)
+        {
+            collider = GetComponent<Collider2D>();
+        }
+        
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+    }
+
+
+}
