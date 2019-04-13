a416594838c44e340c5e5a102fd1cfc294d5d7d1
qyj <qyjbeijing@163.com>
2019-04-13 12:32:55 +0800

    陨石整体完成

--- MagneticGangsta/Assets/Scripts/Items/MeteoriteFall.cs
+++ MagneticGangsta/Assets/Scripts/Items/MeteoriteFall.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MeteoriteFall : PlayerFunctionBase
 {
+    public override string Name { get { return "MeteoriteFall"; } }
     public Vector2 TargetPoint;
 
     public float Speed = 1f;
 
     public float SpeedAdd = 3f;
 
+    public event UnityAction OnBoom;
+
+    private bool isBoomed = false;
     private Vector2 Pos2Target
     {
         get
@@ -26,21 +31,41 @@ public class MeteoriteFall : PlayerFunctionBase
         {
             collider = GetComponent<Collider2D>();
         }
-        transform.forward = Pos2Target;
+        transform.up = Pos2Target;
         collider.isTrigger = true;
     }
 
     public override void PlayerLoop()
     {
-        Speed += SpeedAdd * Time.deltaTime;
-        transform.position += (Vector3)Pos2Target.normalized * Speed * Time.deltaTime;
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isBoomed)
+        {
+            Speed += SpeedAdd * Time.fixedDeltaTime;
+            PlayerRigidbody2D.velocity = (Vector3)Pos2Target.normalized * Speed;
+            transform.up = PlayerRigidbody2D.velocity;
+        }
+        else
+        {
+            PlayerRigidbody2D.velocity = Vector3.zero;
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 10)
+        if (collision.gameObject.layer == 10 && collider.IsTouching(collision))
         {
-            Destroy(gameObject);
+            OnBoom?.Invoke();
+            isBoomed = true;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 }
